44938884d8f2fc7662f0d4fdee0ab077a41dbd22
Bobby Tyner <initium.thoth@gmail.com>
2015-02-17 12:57:18 -0800

    implementing dev in prod

--- main.c
+++ main.c
@@ -10,9 +10,11 @@ Description:
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 /*declarations & globals if not included*/
 void sprinkle_fairy_dust_over_that(void);
+bool just_fix_it_on_production(void);
 
 int main(int argc,char *argv[]){
   /*inspect arguments*/
@@ -20,6 +22,13 @@ int main(int argc,char *argv[]){
 
   /*core program*/
 	sprinkle_fairy_dust_over_that();
+
+	if(just_fix_it_on_production()){
+		printf("You'll go far!");
+	}else{
+		fprintf(stderr,"I'm sorry Dave.  I am afraid I can't do that.");
+		exit(EXIT_FAILURE);
+	}
 
   exit(EXIT_SUCCESS);
 }
